ac3568a369424086a6bb630d2e7ae50ca90be1b3
AlexLin1234 <skyloft0629@gmail.com>
2023-05-17 17:38:59 +0900

    Finished lesson 2.

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+//custom components
+import CustomForm from './component/CustomForm'
 
 function App() {
   const [count, setCount] = useState(0)
-  
+  const addTask = (task) => {
+
+    console.log(task)
+  }
   return (
-    <div className = "App">
-      
+    <div className = "container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask = {addTask} />
     </div>
   )
 }
